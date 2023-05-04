901b27bdd86dea0c9683b60677eeaa4312ff44af
martinrumisek <mrumisek@seznam.cz>
2023-05-04 18:27:26 +0200

    Přidána práce s databázi. Přidávání, odebírání, úprava. (25.4.2023 - 2.5.2023)

--- src/MainPage.java
+++ src/MainPage.java
@@ -1,15 +1,19 @@
+import net.proteanit.sql.DbUtils;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
 
 public class MainPage extends JFrame {
     private JLabel labelNav;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
+    private JButton btnMain;
+    private JButton btnSeznam;
+    private JButton btnAddPeople;
+    private JButton btnNote;
     private JLabel textNav;
     private JPanel NavbarPanel;
     private JPanel mainPanel1;
@@ -22,14 +26,65 @@ public class MainPage extends JFrame {
     private JPanel tab3;
     private JPanel tab4;
     private JPanel TabPanel;
-    private JTable tableList;
+    private JButton btnDeleteForm;
+    private JTextField textName;
+    private JTextField textEmail;
+    private JTextField textPhone;
+    private JButton btnSave;
+    private JTextPane textAddress;
+    private JComboBox chosseTrida;
+    private JTable tableZaci;
+    private JTextField findDataTable;
+    private JButton btnFindData;
+    private JButton addData;
+    private JButton btnDeleteData;
+    private JButton btnRepairData;
+    private JTextArea textArea1;
+    private JButton button2;
+    private JPanel tab5;
+    private JTabbedPane tabbedPane1;
+    private JTabbedPane tabbedPane2;
+    private JTabbedPane tabbedPane3;
+    private JTabbedPane tabbedPane4;
+    private JTabbedPane tabbedPane5;
+    private JTabbedPane tabbedPane6;
 
     public MainPage() {
         tab1.setVisible(true);
         tab2.setVisible(false);
         tab3.setVisible(false);
         tab4.setVisible(false);
+        connectData();
+        comboBox();
+        viewDataTable();
+        btnRepairData.setVisible(false);
+        btnDeleteData.setVisible(false);
+        tableZaci.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
 
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
         closeWindow.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -42,7 +97,7 @@ public class MainPage extends JFrame {
                 minimizeWindow();
             }
         });
-        button1.addActionListener(new ActionListener() {
+        btnMain.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 tab1.setVisible(true);
@@ -51,25 +106,35 @@ public class MainPage extends JFrame {
                 tab4.setVisible(false);
             }
         });
-        button2.addActionListener(new ActionListener() {
+        btnSeznam.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 tab1.setVisible(false);
                 tab2.setVisible(true);
                 tab3.setVisible(false);
                 tab4.setVisible(false);
+                findDataTable.setText("");
             }
         });
-        button3.addActionListener(new ActionListener() {
+        btnAddPeople.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 tab1.setVisible(false);
                 tab2.setVisible(false);
                 tab3.setVisible(true);
                 tab4.setVisible(false);
+                btnDeleteData.setVisible(false);
+                btnRepairData.setVisible(false);
+                btnSave.setVisible(true);
+                btnDeleteForm.setVisible(true);
+                textName.setText("");
+                chosseTrida.setSelectedItem("...");
+                textEmail.setText("");
+                textPhone.setText("");
+                textAddress.setText("");
             }
         });
-        button4.addActionListener(new ActionListener() {
+        btnNote.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 tab1.setVisible(false);
@@ -78,13 +143,263 @@ public class MainPage extends JFrame {
                 tab4.setVisible(true);
             }
         });
+        btnSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                savaData();
+            }
+        });
+        btnDeleteForm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                deleteFormData();
+            }
+        });
+
+        btnFindData.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                repairData();
+            }
+        });
+        btnRepairData.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                repairDataForm();
+            }
+        });
+        btnDeleteData.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                deleteData();
+            }
+        });
+        addData.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tab1.setVisible(false);
+                tab2.setVisible(false);
+                tab3.setVisible(true);
+                tab4.setVisible(false);
+                btnDeleteData.setVisible(false);
+                btnRepairData.setVisible(false);
+                btnSave.setVisible(true);
+                btnDeleteForm.setVisible(true);
+                textName.setText("");
+                chosseTrida.setSelectedItem("...");
+                textEmail.setText("");
+                textPhone.setText("");
+                textAddress.setText("");
+            }
+        });
     }
-    void closeWindow(){
+    public void closeWindow(){
         WindowEvent closeProject = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
         Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(closeProject);
     }
-    void minimizeWindow(){
+    public void minimizeWindow(){
         setState(Frame.ICONIFIED);
+    }
+    public void comboBox(){
+        chosseTrida.addItem("...");
+        chosseTrida.addItem("1.A");
+        chosseTrida.addItem("1.B");
+        chosseTrida.addItem("1.C");
+        chosseTrida.addItem("1.D");
+        chosseTrida.addItem("2.A");
+        chosseTrida.addItem("2.B");
+        chosseTrida.addItem("2.C");
+        chosseTrida.addItem("2.D");
+        chosseTrida.addItem("3.A");
+        chosseTrida.addItem("3.B");
+        chosseTrida.addItem("3.C");
+        chosseTrida.addItem("3.D");
+        chosseTrida.addItem("4.A");
+        chosseTrida.addItem("4.B");
+        chosseTrida.addItem("4.C");
+        chosseTrida.addItem("4.D");
+    }
+    Connection con;
+    PreparedStatement pst;
+    public void connectData(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/projektjava","root","");
+            System.out.println("připojeno k databází funguje!");
+        }catch (ClassNotFoundException ex){
+            ex.printStackTrace();
+        }
+        catch (SQLException ex){
+            ex.printStackTrace();
+        }
+    }
+    public void savaData(){
+        String name, trida, email, phone, address;
+        name = textName.getText();
+        trida = chosseTrida.getSelectedItem().toString();
+        email = textEmail.getText();
+        phone = textPhone.getText();
+        address = textAddress.getText();
+                try{
+                    pst = con.prepareStatement("insert into zaci(name, class, email, phone, address)values(?,?,?,?,?)");
+                    pst.setString(1, name);
+                    pst.setString(2, trida);
+                    pst.setString(3, email);
+                    pst.setString(4, phone);
+                    pst.setString(5, address);
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(null, "Žák byl úspěšně přidán.");
+
+                    textName.setText("");
+                    chosseTrida.setSelectedItem("...");
+                    textEmail.setText("");
+                    textPhone.setText("");
+                    textAddress.setText("");
+                    viewDataTable();
+                    tab3.setVisible(false);
+                    tab2.setVisible(true);
+
+
+                }catch (SQLException e1){
+                    e1.printStackTrace();
+                }
+    }
+    // udělat rozdělanou zkoušku..
+    public void deleteFormData(){
+        /// Zkouška pro zajistění, aby ve formuláři bylo něco napsané.
+        if (textName.getSelectedText() == ""){
+            JOptionPane.showMessageDialog(null, "je plné");
+        }else {
+            JOptionPane.showMessageDialog(null, "je prázdné");
+        }
+        textName.setText("");
+        chosseTrida.setSelectedItem("...");
+        textEmail.setText("");
+        textPhone.setText("");
+        textAddress.setText("");
+    }
+    public void viewDataTable(){
+        //String column_names[]= {"Serial Number","Medicine Name","Dose","Frequency"};
+        String col[] = {"ID", "Jméno a příjmení", "Třída", "E-mail", "Telefonní číslo", "Adresa"};
+        try{
+            pst = con.prepareStatement("select * from zaci");
+            ResultSet rs = pst.executeQuery();
+            tableZaci.setModel(DbUtils.resultSetToTableModel(rs));
+        }catch (SQLException eTable){
+            eTable.printStackTrace();
+        }
+    }
+    public void repairData(){
+        String index = findDataTable.getText();
+        //JOptionPane.showMessageDialog(null, "Funkce tabulky." + index);
+        try{
+
+            //JOptionPane.showMessageDialog(null, index);
+            pst = con.prepareStatement("select name,class,email,phone,address from zaci where id = ?");
+            pst.setString(1, index);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next() == true){
+                btnDeleteData.setVisible(true);
+                btnRepairData.setVisible(true);
+                btnSave.setVisible(false);
+                btnDeleteForm.setVisible(false);
+                String name = rs.getString(1);
+                String trida = rs.getString(2);
+                String email = rs.getString(3);
+                String phone = rs.getString(4);
+                String address = rs.getString(5);
+
+                textName.setText(name);
+                chosseTrida.setSelectedItem(trida);
+                textEmail.setText(email);
+                textPhone.setText(phone);
+                textAddress.setText(address);
+
+                tab2.setVisible(false);
+                tab3.setVisible(true);
+
+            }else{
+                pst = con.prepareStatement("select name,class,email,phone,address from zaci where name = ?");
+                pst.setString(1, index);
+                ResultSet rsa = pst.executeQuery();
+                if (rsa.next() == true){
+                    btnDeleteData.setVisible(true);
+                    btnRepairData.setVisible(true);
+                    btnSave.setVisible(false);
+                    btnDeleteForm.setVisible(false);
+                    String name = rsa.getString(1);
+                    String trida = rsa.getString(2);
+                    String email = rsa.getString(3);
+                    String phone = rsa.getString(4);
+                    String address = rsa.getString(5);
+
+                    textName.setText(name);
+                    chosseTrida.setSelectedItem(trida);
+                    textEmail.setText(email);
+                    textPhone.setText(phone);
+                    textAddress.setText(address);
+
+                    tab2.setVisible(false);
+                    tab3.setVisible(true);
+
+                }else{
+                    JOptionPane.showMessageDialog(null, "Daný žák nebyl nalezen!");
+                }
+            }
+        }catch (SQLException e){
+
+        }
+    }
+    public void repairDataForm(){
+        String id, name, trida, email, phone, address;
+        name = textName.getText();
+        trida = chosseTrida.getSelectedItem().toString();
+        email = textEmail.getText();
+        phone = textPhone.getText();
+        address = textAddress.getText();
+        id = findDataTable.getText();
+        try{
+           pst = con.prepareStatement("update zaci set name = ?,class = ?, email = ?, phone = ?, address = ? where id = ? ");
+           pst.setString(1, name);
+            pst.setString(2, trida);
+            pst.setString(3, email);
+            pst.setString(4, phone);
+            pst.setString(5, address);
+            pst.setString(6, id);
+            pst.executeUpdate();
+            viewDataTable();
+            textName.setText("");
+            chosseTrida.setSelectedItem("...");
+            textEmail.setText("");
+            textPhone.setText("");
+            textAddress.setText("");
+            findDataTable.setText("");
+            tab3.setVisible(false);
+            tab2.setVisible(true);
+        }catch (SQLException e1){
+
+        }
+    }
+    public void deleteData(){
+        String id;
+        id = findDataTable.getText();
+        try{
+            pst = con.prepareStatement("delete from zaci where id = ?");
+            pst.setString(1, id);
+            pst.executeUpdate();
+            viewDataTable();
+            textName.setText("");
+            chosseTrida.setSelectedItem("...");
+            textEmail.setText("");
+            textPhone.setText("");
+            textAddress.setText("");
+            findDataTable.setText("");
+            tab3.setVisible(false);
+            tab2.setVisible(true);
+
+        }catch (SQLException e){
+
+        }
     }
     public static void main(String[] args) {
         MainPage page = new MainPage();
